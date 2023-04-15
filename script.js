7bcf8fc0e5323dd425f6fa3696e345c74a2ae991
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-04-15 06:30:50 +0500

    complete

--- script.js
+++ script.js
@@ -20,17 +20,71 @@
 
 // photos
 
-const imagesElem = document.getElementById("images");
+// const imagesElem = document.getElementById("images");
 
-fetch('https://jsonplaceholder.typicode.com/photos')
-      .then(function(response){
-        return response.json();
-      })
-      .then(function(data){
-        data.forEach(function(images){
-            const url = images.url;
-            const imgElem = document.createElement("img");
-            imgElem.src = url
-            imagesElem.appendChild(imgElem);
+// fetch('https://jsonplaceholder.typicode.com/photos')
+//       .then(function(response){
+//         return response.json();
+//       })
+//       .then(function(data){
+//         data.forEach(function(images){
+//             const url = images.url;
+//             const imgElem = document.createElement("img");
+//             imgElem.src = url
+//             imagesElem.appendChild(imgElem);
+//         })
+//       });
+
+
+      // fetch('https://jsonplaceholder.typicode.com/posts', {
+      //   method: "post"
+      // })
+      // .then(function(response){
+      //   return response.json();
+      // })
+      // .then(function(data){
+      //   console.log(data);
+      // });
+
+      fetch('https://dummyjson.com/products/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title: 'BMW Pencil',
+          /* other product data */
+          description: 'This is a good  product'
         })
-      });
+      })
+      .then(res => res.json())
+      .then(console.log);
+      //// data update krny k liay
+
+      fetch('https://dummyjson.com/products/1', {
+        method: 'PUT', /* or PATCH */
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title: 'iPhone Galaxy +1'
+        })
+      })
+      .then(res => res.json())
+      .then(console.log);
+
+//// data ko delete krny k liay
+
+
+fetch('https://dummyjson.com/products/1', {
+  method: 'DELETE',
+})
+.then(res => res.json())
+.then(console.log);
+
+
+
+
+
+
+
+
+
+
+
